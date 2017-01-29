546352e40a810dc2aab9d72cd637bcb806490092
Brandon Wong <b.lh.wong@gmail.com>
2017-01-28 18:50:46 -0800

    pseudo code recursion of stringify function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,70 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // numbers(ints, floats), null, boolean, string, array, object
+  // no functions, no undefined
+  // use recursion for nested arrays or nested objects
+
+  // create variable result string
+  // for(var key in obj)...
+    // if(obj[key] is Array or Object)
+      // use recursion
+  // output result
+  var result = "{";
+  for (var key in obj) {
+    if (Array.isArray(obj[key])) {
+        //for(var i=0; i<obj[key].length; i++) {
+          // do something use recursion
+          //call stringifyJSON(obj[key]);
+        }
+      } else if (typeof(obj[key]) === "object"){
+      for(var key2 in obj[key]) {
+        // do something use recursion
+      } else {
+        result = result + //something;
+      }
+    }
+  }
+  return result + "}";
 };
+/*
+var stringifiableObjects = [
+  9,
+  null,
+  true,
+  false,
+  'Hello world',
+  [],
+  [8],
+  ['hi'],
+  [8, 'hi'],
+  [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+  [8, [[], 3, 4]],
+  [[[['foo']]]],
+  {},
+  {'a': 'apple'},
+  {'foo': true, 'bar': false, 'baz': null},
+  {'boolean, true': true, 'boolean, false': false, 'null': null },
+  // basic nesting
+  {'a': {'b': 'c'}},
+  {'a': ['b', 'c']},
+  [{'a': 'b'}, {'c': 'd'}],
+  {'a': [], 'c': {}, 'b': true}
+];
+*/
+
+// var myObj = { "name":"John", "age":31, "city":"New York", "grades":[90, 90, 90, [100, 100, 100]] };
+// console.log(JSON.stringify(myObj));
+// {"name":"John","age":31,"city":"New York"}
+
+
+/*    if (Array.isArray(array)) {
+      for(var i=0; i<array.length; i++) {
+        do something
+      }
+    } else {
+      for(var key in object) {
+        do something
+      }
+    }
+*/
