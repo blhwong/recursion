5b88614a936112cde30cd90e64089b37f7910c83
Brandon Wong <b.lh.wong@gmail.com>
2017-01-29 12:11:52 -0800

    Pass test for JSON stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,106 +4,28 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-  // numbers(ints, floats), null, boolean, string, array, object
-  // no functions, no undefined
-  // use recursion for nested arrays or nested objects
-
-  // create variable result string
-  // for(var key in obj)...
-    // if(obj[key] is Array or Object)
-      // use recursion
-  // output result
-
   if((typeof(obj)!=="function") && (obj!==undefined)) {
       if (obj===null) {
          return "null";
       } else if (typeof(obj)==="string") {
         return '"'+obj+'"';
       } else if (Array.isArray(obj)) {
-        // return stringifyJSON(obj[n]);
         var result=[];
         for (var i = 0; i < obj.length; i++) {
           result.push(stringifyJSON(obj[i]));
         }
         return "[" +result.join(",")+ "]";
+      } else if (typeof(obj)==="object") {
+        var result=[];
+        for (var i in obj) {
+          if((typeof(obj[i])!=="function") && (obj[i]!==undefined)) {
+            result.push('"'+i+'"'+":"+stringifyJSON(obj[i]));
+          } else {
+            continue;
+          }
+        }
+        return "{" +result.join(",")+ "}";
       }
       return obj.toString();
   }
 }
-
-
-/*
-
-
-console.log(obj);
-  var accumulator="";
-  _.reduce(obj, function(current, key, accumulator) {
-    if((typeof(current)!=="function") && (current!==undefined)) {
-      if (current===null) {
-         accumulator = accumulator + "null";
-      }
-      accumualator = accumulator + current;
-    }
-  }, accumulator);
-  return accumulator;
-
-
-var result = "{";
-  for (var key in obj) {
-    if (Array.isArray(obj[key])) {
-        //for(var i=0; i<obj[key].length; i++) {
-          // do something use recursion
-          //call stringifyJSON(obj[key]);
-        }
-      } else if (typeof(obj[key]) === "object"){
-      for(var key2 in obj[key]) {
-        // do something use recursion
-      } else {
-        result = result + //something;
-      }
-    }
-  }
-  return result + "}";
-};*/
-/*
-var stringifiableObjects = [
-  9,
-  null,
-  true,
-  false,
-  'Hello world',
-  [],
-  [8],
-  ['hi'],
-  [8, 'hi'],
-  [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
-  [8, [[], 3, 4]],
-  [[[['foo']]]],
-  {},
-  {'a': 'apple'},
-  {'foo': true, 'bar': false, 'baz': null},
-  {'boolean, true': true, 'boolean, false': false, 'null': null },
-  // basic nesting
-  {'a': {'b': 'c'}},
-  {'a': ['b', 'c']},
-  [{'a': 'b'}, {'c': 'd'}],
-  {'a': [], 'c': {}, 'b': true}
-];
-*/
-
-// var myObj = { "name":"John", "age":31, "city":"New York", "grades":[90, 90, 90, [100, 100, 100]] };
-// console.log(JSON.stringify(myObj));
-// {"name":"John","age":31,"city":"New York"}
-
-
-/*    if (Array.isArray(array)) {
-      for(var i=0; i<array.length; i++) {
-        do something
-      }
-    } else {
-      for(var key in object) {
-        do something
-      }
-    }
-*/
