690bce0f9b541ca01ba831f15438c40f029af02e
Brandon Wong <b.lh.wong@gmail.com>
2017-01-29 10:32:22 -0800

    Refactor using reduce for non nested items

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -14,7 +14,21 @@ var stringifyJSON = function(obj) {
     // if(obj[key] is Array or Object)
       // use recursion
   // output result
-  var result = "{";
+  //create empty result string (accumulator)
+  var accumulator="";
+  _.reduce(obj, function(current, key, accumulator) {
+    if((typeof(current)!=="function") || (current!==undefined)) {
+      if (current===null) {
+         accumulator = accumulator + "null";
+      }
+      accumualator = accumulator + current;
+    }
+  }, accumulator);
+  return accumulator;
+}
+
+
+/*  var result = "{";
   for (var key in obj) {
     if (Array.isArray(obj[key])) {
         //for(var i=0; i<obj[key].length; i++) {
@@ -30,8 +44,7 @@ var stringifyJSON = function(obj) {
     }
   }
   return result + "}";
-};
-
+};*/
 /*
 var stringifiableObjects = [
   9,
@@ -56,21 +69,7 @@ var stringifiableObjects = [
   [{'a': 'b'}, {'c': 'd'}],
   {'a': [], 'c': {}, 'b': true}
 ];
->> [9,null,true,false,"Hello world",[],[8],["hi"],[8,"hi"],[1,0,-1,-0.3,0.3,1343.32,3345,0.00011999999999999999],[8,[[],3,4]],[[[["foo"]]]],{},{"a":"apple"},{"foo":true,"bar":false,"baz":null},{"boolean, true":true,"boolean, false":false,"null":null},{"a":{"b":"c"}},{"a":["b","c"]},[{"a":"b"},{"c":"d"}],{"a":[],"c":{},"b":true}]
-
-
-var stringifyJSON = function(obj) {
-  //create empty result string (accumulator)
-  _.reduce(obj, function(current, key, accumulator) {
-    // if not a function or undefined
-      // if null
-        // concat null into accumulator
-      concat current into accumulator
-
-  }, accumulator);
-  return accumulator;
-}
-
+*/
 
 // var myObj = { "name":"John", "age":31, "city":"New York", "grades":[90, 90, 90, [100, 100, 100]] };
 // console.log(JSON.stringify(myObj));
