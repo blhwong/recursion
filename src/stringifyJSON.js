744f4b690970765ff584f4962f144c1f4df12c16
Brandon Wong <b.lh.wong@gmail.com>
2017-02-05 00:44:23 -0800

    Clean up code and eliminate redundancies

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,26 +4,22 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  if ((typeof obj !== 'function') && (obj !== undefined)) {
-      if (obj === null) {
-         return "null";
-      } else if (typeof obj === 'string') {
-        return '"' + obj + '"';
-      } else if (Array.isArray(obj)) {
-        var result = [];
-        for (var i = 0; i < obj.length; i++) {
-          result.push(stringifyJSON(obj[i]));
-        }
-        return '[' + result.join(',') + ']';
-      } else if (typeof obj === 'object') {
-        var result = [];
-        for (var i in obj) {
-          if((typeof obj[i] !== 'function') && (obj[i] !== undefined)) {
-            result.push('"' + i + '"' + ":" + stringifyJSON(obj[i]));
-          }
-        }
-        return '{' + result.join(',') + '}';
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+    var result = [];
+    for (var i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result.join(',') + ']';
+  } else if (typeof obj === 'object' && obj) {
+    var result = [];
+    for (var i in obj) {
+      if((typeof obj[i] !== 'function') && (obj[i] !== undefined)) {
+        result.push('"' + i + '"' + ":" + stringifyJSON(obj[i]));
       }
-      return obj.toString();
+    }
+    return '{' + result.join(',') + '}';
   }
+  return '' + obj;
 }
